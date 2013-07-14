f3b25f92447f2e4898a6e0d91f79ae3eb9f6e3e8
Andrey Nikitin <ubuntu@ip-172-31-7-185.us-west-2.compute.internal>
2013-07-14 12:34:40 +0000

    Added web with index.html

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
-fs.readFileSync(index.html);
+var index = fs.readFileSync(index.html);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-  response.send(buf.toString());
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
